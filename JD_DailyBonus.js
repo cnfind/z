e079c90c1920455dee685958e41255944b6d11b9
cnfind <75772299+cnfind@users.noreply.github.com>
2021-12-10 15:34:08 +0800

    Add files via upload

--- JD_DailyBonus.js
+++ JD_DailyBonus.js
@@ -46,7 +46,7 @@ var Key = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用jso
 
 var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用json串数据  ↓
 
-var OtherKey = `[{"cookie":"pt_key=AAJhmRU4ADBOe6FbLH_mvGhnvbGoxbam8aWTbGAFSnxceNUUwxd4aYsuhuBb8dY0jo4IzNDhq94;pt_pin=jd_6129a3066c0b6;","jrBody":""}]`; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
+var OtherKey = ``; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 /*以下样例为双账号("cookie"为必须,其他可选), 第一个账号仅包含Cookie, 第二个账号包含Cookie和金融签到Body: 
 
